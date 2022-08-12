c1fb23367daecc5f5b4d541513776ecc8f793cfd
TravisMoleski <tm769613@ohio.edu>
2022-08-12 16:08:07 -0400

    working with ros, need to specify topic in *launch_demo.sh*

--- demo.py
+++ demo.py
@@ -7,6 +7,8 @@ import scipy.special, tqdm
 import numpy as np
 import torchvision.transforms as transforms
 from data.dataset import LaneTestDataset
+from data.dataset import LaneTestDatasetRos
+import time
 from data.constant import culane_row_anchor, tusimple_row_anchor
 
 if __name__ == "__main__":
@@ -23,7 +25,8 @@ if __name__ == "__main__":
         cls_num_per_lane = 56
     elif cfg.dataset == 'Live':
         cls_num_per_lane = 56
-
+    elif cfg.dataset == 'ROS':
+        cls_num_per_lane = 56
     else:
         raise NotImplementedError
 
@@ -46,70 +49,85 @@ if __name__ == "__main__":
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
-    if cfg.dataset == 'CULane':
-        splits = ['test0_normal.txt', 'test1_crowd.txt', 'test2_hlight.txt', 'test3_shadow.txt', 'test4_noline.txt', 'test5_arrow.txt', 'test6_curve.txt', 'test7_cross.txt', 'test8_night.txt']
-        datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root,split),img_transform = img_transforms) for split in splits]
-        img_w, img_h = 1640, 590
-        row_anchor = culane_row_anchor
 
-    elif cfg.dataset == 'Tusimple':
-        splits = ['test.txt']
-        datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root, split),img_transform = img_transforms) for split in splits]
-        img_w, img_h = 1280, 720
-        row_anchor = tusimple_row_anchor
+    if cfg.dataset == 'ROS':
+        import rospy 
+        rospy.init_node('ros_ultrafast_lane', anonymous=True)
+        splits = ['topic.txt']
+        datasets = [LaneTestDatasetRos(cfg.ros_topic,img_transform = img_transforms) for split in splits]
+        r = rospy.Rate(1)
+        r.sleep()
 
-    elif cfg.dataset == 'Live':
-        splits = ['test.txt']
-        datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root,split),img_transform = img_transforms) for split in splits]
-        print((datasets.__getitem__(0)[0][1]))
-        loaded_im = cv2.imread(datasets.__getitem__(0)[0][1])
-        img_w = loaded_im.shape[1]
-        img_h = loaded_im.shape[0]
-        row_anchor = tusimple_row_anchor
+    while not rospy.is_shutdown():
+        if cfg.dataset == 'CULane':
+            splits = ['test0_normal.txt', 'test1_crowd.txt', 'test2_hlight.txt', 'test3_shadow.txt', 'test4_noline.txt', 'test5_arrow.txt', 'test6_curve.txt', 'test7_cross.txt', 'test8_night.txt']
+            datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root,split),img_transform = img_transforms) for split in splits]
+            img_w, img_h = 1640, 590
+            row_anchor = culane_row_anchor
 
-    else:
-        raise NotImplementedError
-    for split, dataset in zip(splits, datasets):
-        loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle = False, num_workers=1)
-        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        print(split[:-3]+'avi')
-        # vout = cv2.VideoWriter(split[:-3]+'avi', fourcc , 30.0, (img_w, img_h))
-        for i, data in enumerate(tqdm.tqdm(loader)):
-            imgs, names = data
-            imgs = imgs.cuda()
-            with torch.no_grad():
-                out = net(imgs)
+        elif cfg.dataset == 'Tusimple':
+            splits = ['test.txt']
+            datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root, split),img_transform = img_transforms) for split in splits]
+            img_w, img_h = 1280, 720
+            row_anchor = tusimple_row_anchor
 
-            col_sample = np.linspace(0, 800 - 1, cfg.griding_num)
-            col_sample_w = col_sample[1] - col_sample[0]
-
-
-            out_j = out[0].data.cpu().numpy()
-            out_j = out_j[:, ::-1, :]
-            prob = scipy.special.softmax(out_j[:-1, :, :], axis=0)
-            idx = np.arange(cfg.griding_num) + 1
-            idx = idx.reshape(-1, 1, 1)
-            loc = np.sum(prob * idx, axis=0)
-            out_j = np.argmax(out_j, axis=0)
-            loc[out_j == cfg.griding_num] = 0
-            out_j = loc
-
-            # import pdb; pdb.set_trace()
-            vis = cv2.imread(os.path.join(cfg.data_root,names[0]))
-            for i in range(out_j.shape[1]):
-                if np.sum(out_j[:, i] != 0) > 2:
-                    for k in range(out_j.shape[0]):
-                        if out_j[k, i] > 0:
-                            ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1, int(img_h * (row_anchor[cls_num_per_lane-1-k]/288)) - 1 )
-                            cv2.circle(vis,ppp,5,(0,255,0),-1)
-
-            vis = cv2.resize(vis, (640,480))
-            cv2.imshow('vis',vis)
-            save_path = cfg.save_loc + '/some_name.jpg'
-            print(save_path)
-            cv2.imwrite(str(save_path),vis)
-            cv2.waitKey(0)
-            
-            # vout.write(vis)
+        elif cfg.dataset == 'Live':
+            splits = ['test.txt']
+            datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root,split),img_transform = img_transforms) for split in splits]
+            print((datasets.__getitem__(0)[0][1]))
+            loaded_im = cv2.imread(datasets.__getitem__(0)[0][1])
+            img_w = loaded_im.shape[1]
+            img_h = loaded_im.shape[0]
+            row_anchor = tusimple_row_anchor
         
-        # vout.release()
+        elif cfg.dataset == 'ROS':
+            loaded_im = datasets.__getitem__(0)[0][0]
+            img_w = datasets[0].cv_image.shape[1]
+            img_h = datasets[0].cv_image.shape[0]
+            row_anchor = tusimple_row_anchor
+
+        else:
+            raise NotImplementedError
+
+        for split, dataset in zip(splits, datasets):
+            loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle = False, num_workers=1)
+            for i, data in enumerate(tqdm.tqdm(loader)):
+                imgs, names = data
+                imgs = imgs.cuda()
+                with torch.no_grad():
+                    out = net(imgs)
+
+                col_sample = np.linspace(0, 800 - 1, cfg.griding_num)
+                col_sample_w = col_sample[1] - col_sample[0]
+
+                out_j = out[0].data.cpu().numpy()
+                out_j = out_j[:, ::-1, :]
+                prob = scipy.special.softmax(out_j[:-1, :, :], axis=0)
+                idx = np.arange(cfg.griding_num) + 1
+                idx = idx.reshape(-1, 1, 1)
+                loc = np.sum(prob * idx, axis=0)
+                out_j = np.argmax(out_j, axis=0)
+                loc[out_j == cfg.griding_num] = 0
+                out_j = loc
+
+                # import pdb; pdb.set_trace()
+                # vis = cv2.imread(os.path.join(cfg.data_root,names[0]))
+                for i in range(out_j.shape[1]):
+                    if np.sum(out_j[:, i] != 0) > 2:
+                        for k in range(out_j.shape[0]):
+                            if out_j[k, i] > 0:
+                                ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1, int(img_h * (row_anchor[cls_num_per_lane-1-k]/288)) - 1 )
+                                cv2.circle(datasets[0].cv_image,ppp,10,(0,0,255),-1)
+
+                vis = cv2.resize(datasets[0].cv_image, (640,480))
+                cv2.imshow('vis',vis)
+                # save_path = cfg.save_loc + '/some_name.jpg'
+                # print(save_path)
+                # cv2.imwrite(str(save_path),vis)
+                r = rospy.Rate(1000)
+                r.sleep()
+                cv2.waitKey(1)
+                
+                # vout.write(vis)
+            
+            # vout.release()
