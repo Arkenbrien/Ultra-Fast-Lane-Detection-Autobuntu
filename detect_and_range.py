1e137f0bafe305388a9ade7865ae828ac44bee46
TravisMoleski <tm769613@ohio.edu>
2022-08-25 17:01:43 -0400

    nearest point to lines with lidar done, need distances

--- detect_and_range.py
+++ detect_and_range.py
@@ -15,6 +15,7 @@ from lidar2cam_projection.msg import pixel_ranges
 from lidar2cam_projection.msg import pixel_range
 
 import rospy
+from shapely import geometry
 
 class pixel_range_sub(object):
     def __init__(self):
@@ -23,12 +24,15 @@ class pixel_range_sub(object):
     def get_pixel_range(self, data):
         self.pixel_vector =  data
 
+def distance(point1,point2):
+    return ((point1[0] - point2[0])**2 + (point1[1]-point2[1])**2)**(1/2)
 
 if __name__ == "__main__":
     
 
     rospy.init_node('pixel_listener', anonymous=True)
     pixel_range_dat = pixel_range_sub()
+    pixel_threshold = 20
     time.sleep(1)
 
     torch.backends.cudnn.benchmark = True
@@ -132,22 +136,106 @@ if __name__ == "__main__":
 
                 # import pdb; pdb.set_trace()
                 # vis = cv2.imread(os.path.join(cfg.data_root,names[0]))
+                store_ppp = []
+                line_list = []
                 for i in range(out_j.shape[1]):
+                    line = []
                     if np.sum(out_j[:, i] != 0) > 2:
                         for k in range(out_j.shape[0]):
                             if out_j[k, i] > 0:
                                 ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1, int(img_h * (row_anchor[cls_num_per_lane-1-k]/288)) - 1 )
-                                cv2.circle(datasets[0].cv_image,ppp,10,(0,0,255),-1)
+                                store_ppp.append(ppp)
+                                # cv2.circle(datasets[0].cv_image,ppp,10,(0,0,255),-1)
+                                line.append(ppp)
+                        line_list.append(line)
 
+                min_list=min(line_list, key=len)
+                for point_px in range(len(min_list)):
+                    line_1_start = line_list[0][point_px]
+                    line_2_start = line_list[1][point_px]
+
+                    center_u = (line_1_start[0]+line_2_start[0])/2
+                    center_v = (line_1_start[1]+line_2_start[1])/2
+
+                    cv2.circle(datasets[0].cv_image,(int(line_1_start[0]),int(line_1_start[1])),10,(0,255,255),-1)
+                    cv2.circle(datasets[0].cv_image,(int(line_2_start[0]),int(line_2_start[1])),10,(0,255,255),-1)
+
+                    cv2.circle(datasets[0].cv_image,(int(center_u),int(center_v)),5,(255,255,255),-1)
+
+                store_x_lane_master = []
+                store_y_lane_master = []
                 for point in pixel_info.points:
                     pixel_u = point.u
                     pixel_v = point.v
+
                     pixel_r = point.range
                     pixel_x = point.x
                     pixel_y = point.y
                     pixel_z = point.z
-                    cv2.circle(datasets[0].cv_image ,(int(pixel_u), int(pixel_v)), 1,(255,0,0),-1)
+                    color = (255,0,0)
 
+                    for line in range(len(line_list)):
+                        # line_x = []
+                        # line_y = []
+                        # line_z = []
+                        for point_px in range(len(min_list)):
+                            line_pix = line_list[line][point_px]
+                            distance_px = distance([pixel_u, pixel_v], line_pix)
+
+                            if distance_px < pixel_threshold:
+                                # line_x.append(pixel_x)
+                                # line_y.append(pixel_y)
+                                # line_z.append(pixel_z)
+                                # store_u_lane.append(pixel_u)
+                                # store_v_lane.append(pixel_v)
+                                if line == 0:
+                                    color = (255,0,255)
+                                if line == 1:
+                                    color = (255,255,0)
+                            
+                        # avg_line_x = np.average(line_x)
+                        # avg_line_y = np.average(line_y)
+                        # avg_line_z = np.average(line_z)
+
+                        # print("AVERAGE LINE DISTANCE:", str(line), "IS", avg_line_x, avg_line_y)
+
+
+                    cv2.circle(datasets[0].cv_image ,(int(pixel_u), int(pixel_v)), 1, color,-1)
+
+                # store_x_lane = []
+                # store_y_lane = []
+                # store_u_lane = []
+                # store_v_lane = []
+                # for point in pixel_info.points:
+                #     pixel_u = point.u
+                #     pixel_v = point.v
+
+                #     pixel_r = point.range
+                #     pixel_x = point.x
+                #     pixel_y = point.y
+                #     pixel_z = point.z
+                #     color = (255,0,0)
+
+                #     for line_pix in store_ppp:
+
+                #         distance_px = distance([pixel_u, pixel_v], line_pix)
+                #         if distance_px < pixel_threshold:
+                #             store_x_lane.append(pixel_x)
+                #             store_y_lane.append(pixel_y)
+                #             store_u_lane.append(pixel_u)
+                #             store_v_lane.append(pixel_v)
+                #             color = (0,255,0)
+
+                #     cv2.circle(datasets[0].cv_image ,(int(pixel_u), int(pixel_v)), 1, color,-1)
+
+                # avgx = np.average(store_x_lane)
+                # avgy = np.average(store_y_lane)
+
+                # avgu = np.average(store_u_lane)
+                # avgv = np.average(store_v_lane)
+                # print(avgx,avgy)
+
+                # cv2.putText(datasets[0].cv_image, 'OpenCV', org, font, fontScale, color, thickness, cv2.LINE_AA)
                 vis = cv2.resize(datasets[0].cv_image, (640,480))
                 cv2.imshow('vis',vis)
                 # save_path = cfg.save_loc + '/some_name.jpg'
