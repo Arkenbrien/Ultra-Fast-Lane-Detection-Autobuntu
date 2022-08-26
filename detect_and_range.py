545f679dae4d27f834a544fc15b756e60bbbe468
TravisMoleski <tm769613@ohio.edu>
2022-08-26 10:06:16 -0400

    now displaying x and y deviations from each line

--- detect_and_range.py
+++ detect_and_range.py
@@ -1,3 +1,4 @@
+from audioop import avg
 import torch, os, cv2
 from model.model import parsingNet
 from utils.common import merge_config
@@ -112,6 +113,7 @@ if __name__ == "__main__":
             raise NotImplementedError
 
         pixel_info = pixel_range_dat.pixel_vector
+        extracted_image = datasets[0].cv_image
 
         for split, dataset in zip(splits, datasets):
             loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle = False, num_workers=1)
@@ -136,7 +138,7 @@ if __name__ == "__main__":
 
                 # import pdb; pdb.set_trace()
                 # vis = cv2.imread(os.path.join(cfg.data_root,names[0]))
-                store_ppp = []
+                # store_ppp = []
                 line_list = []
                 for i in range(out_j.shape[1]):
                     line = []
@@ -144,26 +146,24 @@ if __name__ == "__main__":
                         for k in range(out_j.shape[0]):
                             if out_j[k, i] > 0:
                                 ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1, int(img_h * (row_anchor[cls_num_per_lane-1-k]/288)) - 1 )
-                                store_ppp.append(ppp)
-                                # cv2.circle(datasets[0].cv_image,ppp,10,(0,0,255),-1)
+                                # store_ppp.append(ppp)
+                                cv2.circle(extracted_image,ppp,10,(0,255,255),-1)
                                 line.append(ppp)
                         line_list.append(line)
 
-                min_list=min(line_list, key=len)
-                for point_px in range(len(min_list)):
-                    line_1_start = line_list[0][point_px]
-                    line_2_start = line_list[1][point_px]
+                # min_list=min(line_list, key=len)
+                # for point_px in range(len(min_list)):
+                #     line_1_start = line_list[0][point_px]
+                #     line_2_start = line_list[1][point_px]
 
-                    center_u = (line_1_start[0]+line_2_start[0])/2
-                    center_v = (line_1_start[1]+line_2_start[1])/2
+                #     center_u = (line_1_start[0]+line_2_start[0])/2
+                #     center_v = (line_1_start[1]+line_2_start[1])/2
 
-                    cv2.circle(datasets[0].cv_image,(int(line_1_start[0]),int(line_1_start[1])),10,(0,255,255),-1)
-                    cv2.circle(datasets[0].cv_image,(int(line_2_start[0]),int(line_2_start[1])),10,(0,255,255),-1)
+                #     cv2.circle(datasets[0].cv_image,(int(line_1_start[0]),int(line_1_start[1])),10,(0,255,255),-1)
+                #     cv2.circle(datasets[0].cv_image,(int(line_2_start[0]),int(line_2_start[1])),10,(0,255,255),-1)
+                    # cv2.circle(datasets[0].cv_image,(int(center_u),int(center_v)),5,(255,255,255),-1)
 
-                    cv2.circle(datasets[0].cv_image,(int(center_u),int(center_v)),5,(255,255,255),-1)
-
-                store_x_lane_master = []
-                store_y_lane_master = []
+                lidar_point_master = []
                 for point in pixel_info.points:
                     pixel_u = point.u
                     pixel_v = point.v
@@ -175,73 +175,93 @@ if __name__ == "__main__":
                     color = (255,0,0)
 
                     for line in range(len(line_list)):
-                        # line_x = []
-                        # line_y = []
-                        # line_z = []
-                        for point_px in range(len(min_list)):
+                        for point_px in range(len(line_list[line])):
                             line_pix = line_list[line][point_px]
                             distance_px = distance([pixel_u, pixel_v], line_pix)
-
                             if distance_px < pixel_threshold:
-                                # line_x.append(pixel_x)
-                                # line_y.append(pixel_y)
-                                # line_z.append(pixel_z)
-                                # store_u_lane.append(pixel_u)
-                                # store_v_lane.append(pixel_v)
-                                if line == 0:
-                                    color = (255,0,255)
-                                if line == 1:
-                                    color = (255,255,0)
-                            
-                        # avg_line_x = np.average(line_x)
-                        # avg_line_y = np.average(line_y)
-                        # avg_line_z = np.average(line_z)
-
-                        # print("AVERAGE LINE DISTANCE:", str(line), "IS", avg_line_x, avg_line_y)
+                                lidar_point_master.append([pixel_x, pixel_y, pixel_z, pixel_u, pixel_v, line])
+                    cv2.circle(extracted_image,(int(pixel_u), int(pixel_v)), 1, color,-1)
 
 
-                    cv2.circle(datasets[0].cv_image ,(int(pixel_u), int(pixel_v)), 1, color,-1)
+                line1_x = []
+                line1_y = []
+                line1_z = []
+                line1_u = []
+                line1_v = []
 
-                # store_x_lane = []
-                # store_y_lane = []
-                # store_u_lane = []
-                # store_v_lane = []
-                # for point in pixel_info.points:
-                #     pixel_u = point.u
-                #     pixel_v = point.v
+                line2_x = []
+                line2_y = []
+                line2_z = []
+                line2_u = []
+                line2_v = []
 
-                #     pixel_r = point.range
-                #     pixel_x = point.x
-                #     pixel_y = point.y
-                #     pixel_z = point.z
-                #     color = (255,0,0)
 
-                #     for line_pix in store_ppp:
+                for matched_point in range(len(lidar_point_master)):
+                    extracted_point = lidar_point_master[matched_point]
+                    pixel_x =  extracted_point[0]
+                    pixel_y =  extracted_point[1]
+                    pixel_z =  extracted_point[2]
+                    pixel_u =  extracted_point[3]
+                    pixel_v =  extracted_point[4]
+                    line    =  extracted_point[5]
 
-                #         distance_px = distance([pixel_u, pixel_v], line_pix)
-                #         if distance_px < pixel_threshold:
-                #             store_x_lane.append(pixel_x)
-                #             store_y_lane.append(pixel_y)
-                #             store_u_lane.append(pixel_u)
-                #             store_v_lane.append(pixel_v)
-                #             color = (0,255,0)
+                    if line == 0:
+                        color = (255,0,255)
+                        line1_x.append(pixel_x)
+                        line1_y.append(pixel_y)
+                        line1_z.append(pixel_z)
+                        line1_u.append(pixel_u)
+                        line1_v.append(pixel_v)
 
-                #     cv2.circle(datasets[0].cv_image ,(int(pixel_u), int(pixel_v)), 1, color,-1)
+                    elif line == 1:
+                        color = (255,255,0)
+                        line2_x.append(pixel_x)
+                        line2_y.append(pixel_y)
+                        line2_z.append(pixel_z)
+                        line2_u.append(pixel_u)
+                        line2_v.append(pixel_v)
 
-                # avgx = np.average(store_x_lane)
-                # avgy = np.average(store_y_lane)
+                    cv2.circle(extracted_image,(int(pixel_u), int(pixel_v)), 1, color,-1)
 
-                # avgu = np.average(store_u_lane)
-                # avgv = np.average(store_v_lane)
-                # print(avgx,avgy)
+                line1_x_avg = np.average(line1_x)
+                line1_y_avg = np.average(line1_y)
+                line1_z_avg = np.average(line1_z)
+                line1_u_avg = np.average(line1_u)
+                line1_v_avg = np.average(line1_v)
 
-                # cv2.putText(datasets[0].cv_image, 'OpenCV', org, font, fontScale, color, thickness, cv2.LINE_AA)
-                vis = cv2.resize(datasets[0].cv_image, (640,480))
+                line2_x_avg = np.average(line2_x)
+                line2_y_avg = np.average(line2_y)
+                line2_z_avg = np.average(line2_z)
+                line2_u_avg = np.average(line2_u)
+                line2_v_avg = np.average(line2_v)
+
+
+
+                # print("LINE 1 X DEVIATION: ", line1_x_avg)
+                # print("LINE 1 y DEVIATION: ", line1_y_avg)
+                # print("LINE 1 z DEVIATION: ", line1_z_avg)
+
+                # print("LINE 2 X DEVIATION: ", line2_x_avg)
+                # print("LINE 2 y DEVIATION: ", line2_y_avg)
+                # print("LINE 2 z DEVIATION: ", line2_z_avg)
+                # print("\n")
+
+                cv2.putText(extracted_image, "LINE 1 X: "+str(round(line1_x_avg,2)), (int(line1_u_avg), int(line1_v_avg)), cv2.FONT_HERSHEY_SIMPLEX, 
+                                                    1, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(extracted_image, "LINE 1 Y: "+str(round(line1_y_avg,2)), (int(line1_u_avg), int(line1_v_avg+50)), cv2.FONT_HERSHEY_SIMPLEX, 
+                                                    1, (255,255,255), 2, cv2.LINE_AA)
+
+                cv2.putText(extracted_image, "LINE 2: "+str(round(line2_x_avg,2)), (int(line2_u_avg), int(line2_v_avg)), cv2.FONT_HERSHEY_SIMPLEX, 
+                                                    1, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(extracted_image, "LINE 2 Y: "+str(round(line2_y_avg,2)), (int(line2_u_avg), int(line2_v_avg+50)), cv2.FONT_HERSHEY_SIMPLEX, 
+                                                    1, (255,255,255), 2, cv2.LINE_AA)
+
+                vis = cv2.resize(extracted_image, (640,480))
                 cv2.imshow('vis',vis)
                 # save_path = cfg.save_loc + '/some_name.jpg'
                 # print(save_path)
                 # cv2.imwrite(str(save_path),vis)
-                r = rospy.Rate(1000)
+                r = rospy.Rate(100)
                 r.sleep()
                 cv2.waitKey(1)
                 
